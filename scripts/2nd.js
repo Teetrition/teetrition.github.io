017da028bfe42f51644dcec835b1b634fc7616d6
Teetrition <pkj20000716@live.com>
2020-10-12 22:46:17 +0800

    Code fix 20201012

--- scripts/2nd.js
+++ scripts/2nd.js
@@ -1,4 +1,2 @@
 $("#header").load("../header.html");
 $("#footer").load("../footer.html");
-function imagesrc(){document.getElementById("contactmail").src = "../images/mail.png"};
-$(function(){imagesrc();});
