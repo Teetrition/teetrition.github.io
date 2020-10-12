34b10885c80706d2526db99820f5f84704467a5e
Teetrition <pkj20000716@live.com>
2020-10-12 22:40:02 +0800

    Content Update 20201012

--- scripts/2nd.js
+++ scripts/2nd.js
@@ -1,2 +1,4 @@
 $("#header").load("../header.html");
 $("#footer").load("../footer.html");
+function imagesrc(){document.getElementById("contactmail").src = "../images/mail.png"};
+$(function(){imagesrc();});
